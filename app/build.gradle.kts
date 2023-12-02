aaf2ae358d20bb96c8213cd518a4842159a5814f
Muhammad_Shaban <nisarisoftware@gmail.com>
2023-12-02 11:19:28 +0500

    configer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shaban.myapplication"
-        minSdk = 22
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
